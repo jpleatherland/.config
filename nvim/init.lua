0d419435cb7f5883854c2934525594fdeb2c40af
Jack Leatherland <jack.leatherland@qbe.com>
2024-07-19 09:00:38 +0100

    add folding and tabstops to nvim conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,6 +108,11 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false
 
 vim.opt.ruler = false
 
