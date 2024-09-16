bfaff367a23b89b41fcf3389fa5f788ba073fbb8
Jack Leatherland <jack.leatherland@qbe.com>
2024-09-16 16:20:33 +0100

    ts language server

--- nvim/init.lua
+++ nvim/init.lua
@@ -496,7 +496,8 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				tsserver = {},
+				--tsserver = {},
+        ts_ls = {},
 				eslint = {},
 				--
 
